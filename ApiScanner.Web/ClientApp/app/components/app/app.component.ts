cba9187e6906f3123b2ac7089b13feebeaca7150
Andrei Georgescu <ice_howler@yahoo.com>
2017-08-20 16:51:33 +0300

    dynamic page title change

--- ApiScanner.Web/ClientApp/app/components/app/app.component.ts
+++ ApiScanner.Web/ClientApp/app/components/app/app.component.ts
@@ -1,9 +1,32 @@
-import { Component } from '@angular/core';
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/mergeMap';
+
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
 
 @Component({
     selector: 'app',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+    constructor(
+        private router: Router,
+        private titleService: Title,
+        private activatedRoute: ActivatedRoute) { }
+
+    ngOnInit() {
+        this.router.events
+            .filter(event => event instanceof NavigationEnd)
+            .map(() => this.activatedRoute)
+            .map(route => {
+                while (route.firstChild) route = route.firstChild;
+                return route;
+            })
+            .filter(route => route.outlet === 'primary')
+            .mergeMap(route => route.data)
+            .subscribe((event) => this.titleService.setTitle(event['title']));
+    }
 }
