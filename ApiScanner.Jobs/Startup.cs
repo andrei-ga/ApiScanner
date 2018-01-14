9b9cf1f9c0674133d23def1bca9fa5b35617c352
Andrei Georgescu <ice_howler@yahoo.com>
2018-01-14 16:42:01 +0200

    AG: remove unused code

--- ApiScanner.Jobs/Startup.cs
+++ ApiScanner.Jobs/Startup.cs
@@ -42,7 +42,6 @@ namespace ApiScanner.Jobs
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider serviceProvider)
         {
-            //GlobalConfiguration.Configuration.UseActivator(new HangfireActivator(serviceProvider));
             app.UseHangfireDashboard("/hangfire");
             app.UseHangfireServer();
 
